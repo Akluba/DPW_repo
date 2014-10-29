aae017d6d8d5378506832cb1db32fe1b34f38abd
Akluba <anthonykluba@fullsail.edu>
2014-10-28 23:51:58 -0400

    Conditional statement placed to desc patience

--- Lab1_Madlib/madlib.py
+++ Lab1_Madlib/madlib.py
@@ -1,4 +1,4 @@
-# Two conditional statements
+# One conditional statements w/ logical operator
 # At least one function
 
 
@@ -14,6 +14,9 @@ countdown_number = raw_input("Enter a number between 1-10 ")
 height_number = raw_input("Now a number between 1-100 ")
 attack_number = raw_input("And finally, a number between 1-3 ")
 
+# Variable dependant on countdown_number
+patience_level = ''
+
 # Array containing different attacks possible
 attacks = ["head","stomach","butt"]
 
@@ -26,12 +29,21 @@ feet = int(height_number) / 12
 # Mathematical operation 2 - Converting user input number into inches
 inches = int(height_number) % 12
 
+# Condition Statement 1 - discribing villian patience level
+if int(countdown_number) > 8:
+	patience_level = "tolerantly"
+elif int(countdown_number) > 5:
+	patience_level = "anxiously"
+else:
+	patience_level = "eagerly"
+
+ 
 
 # Madlib Section containing -- 
 print "Today started out like any normal day in " + city_name + ". All around are cars driving, birds chirping, children " + verb + ", WAIT! WHAT'S THAT?!"
 
 
-# Madlib Section containing --  
+# Madlib Section containing -- 
 
 
 # Madlib Section containing --  loop
