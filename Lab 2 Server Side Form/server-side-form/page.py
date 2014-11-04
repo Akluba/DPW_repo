6f80acaed9dbed98fb3e8394c641c6cf44a4e769
Akluba <anthonykluba@fullsail.edu>
2014-11-04 13:11:42 -0500

    form select,radio, and 1 text input

--- Lab 2 Server Side Form/server-side-form/page.py
+++ Lab 2 Server Side Form/server-side-form/page.py
@@ -12,3 +12,27 @@ class Page():
 	</head>
 	<body>	
 		"""
+		#var containing html form elements
+		self.form = """
+		<form method="GET">
+		
+			<label for="cheese">What type of cheese?</label>
+			<select name="cheese" autofocus="autofocus">
+				<option value="none">No Cheese</option>
+				<option value="american">American</option>
+				<option value="swiss">Swiss</option>
+				<option value="cheddar">Cheddar</option>
+			</select>  		
+				
+			<label for="where" autofocus="autofocus">For Here or ToGo?</label>
+			<input type="radio" name="where" value="here">Here<br>
+			<input type="radio" name="where" value="togo">ToGo
+			
+			<label for="name">Name For Order?</label> 
+			<input type="text" name="name" autofocus="autofocus" placeholder="What's your name?" />
+		"""
+		#var containing html closing elements
+		self.close = """
+	</body>
+</html>
+		"""
