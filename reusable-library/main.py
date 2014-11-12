ec52814e6d4fed565cc62f2e83e95a1d793a2392
Akluba <anthonykluba@fullsail.edu>
2014-11-12 11:37:16 -0500

    created instance of UserInfo from user inputs

--- reusable-library/main.py
+++ reusable-library/main.py
@@ -19,7 +19,15 @@ class MainHandler(webapp2.RequestHandler):
         #watching for GET method
         #grab variables and write results to browser
         if self.request.GET:
-                	
+        	
+        	#creating an instance of UserInfo to store information
+        	user = UserInfo()
+        	user.sex = self.request.GET['sex']
+        	user.__weight = self.request.GET['weight']
+        	user.drink_type = self.request.GET['drink']
+        	user.__drink_num = self.request.GET['number']
+        	user.__hours = self.request.GET['hours']
+        	
         
         	#write form page to browser
         	self.response.write(result.print_out())
