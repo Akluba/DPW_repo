be3a3b994a78adc99c90763bfed3d6280d0a825f
Akluba <anthonykluba@fullsail.edu>
2014-11-13 13:10:58 -0500

    form page stylized

--- reusable-library/page.py
+++ reusable-library/page.py
@@ -6,24 +6,27 @@ class FormPage(object):
 	<head>
 		<title>BAC</title>
 		<link href="css/style.css" rel="stylesheet" type="text/css" >
+		<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700,200' rel='stylesheet' type='text/css'>
 	</head>
 	<body>
 	"""
 		self.body = """
 		<div id="container">
-			<h1>Form</h1>
+			<div id="page_text">
+				<h1>Having a hard time deciding on whether to drive or not?</h1> 
+				<h2>Use this blood alcohol concentration calculator to find which ride you need to take!</h2>
+			</div>
 			<form method="GET">
+				<div id="questions">
 					<div class="option_box">
 						<label for="sex">Sex?</label>
 						<input type="radio" name="sex" value="male">Male
 						<input type="radio" name="sex" value="female">Female
 					</div>
-					
 					<div class="option_box">
-						<label for="weight">Body weight?</label> 
-						<input type="text" name="weight" />	
+						<label for="weight">Body weight?</label>
+						<input type="text" class="input_num" name="weight" />	
 					</div>
-					
 					<div class="option_box">
 						<label for="drink">Drink of choice?</label>
 						<select name="drink">
@@ -32,23 +35,19 @@ class FormPage(object):
 							<option value="shot">Shots</option>
 						</select> 
 					</div>
-					
 					<div class="option_box">
-						<label for="number">How many drinks?</label> 
-						<input type="text" name="number" />	
+						<label for="number">How many drinks?</label>
+						<input type="text" class="input_num" name="number" />	
 					</div>
-					
 					<div class="option_box">
-						<label for="hours">How many hours since first drink?</label> 
-						<input type="text" name="hours" />	
+						<label for="hours">How many hours since first drink?</label>
+						<input type="text" class="input_num" name="hours" />	
 					</div>
-				
-					
-					
-					<input type="submit" value="Calculate!" />
-				</form>
-			</div>
-			<div id="car_img"></div>"""
+					<input type="submit" id="submit_button" value="Calculate!" />
+				</div>
+			</form>
+		</div>
+		<div id="car_img"></div>"""
 		self.__close = """
 	</body>
 </html>
