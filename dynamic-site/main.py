882342244b37a62a37d2bec8d700a676b315ce0c
Akluba <anthonykluba@Anthonys-MacBook-Pro.local>
2014-11-20 20:17:46 -0500

    object navigation with GET method

--- dynamic-site/main.py
+++ dynamic-site/main.py
@@ -5,13 +5,17 @@ Lab3 Dynamic Site
 11/20/14
 '''
 import webapp2
-from pages import Page
+from pages import Page, ContentPage
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         
-        p = Page()
-        self.response.write(p.print_out())
+        #create instance of FormPage
+        page = ContentPage()
+        #arrays passed to buttons in ContentPage Class
+        page.buttons = [['/?player=1','name1'],['/?player=2','name2'],['/?player=3','name3'],['/?player=4','name4'],['/?player=5','name5']]
+        
+        self.response.write(page.print_out())
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
